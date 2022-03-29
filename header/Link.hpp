2eb2e91fd2c6b9726f30be4787a5eba30f66c407
Thiaggo Ferreira <thiaggo98@gmail.com>
2022-03-29 01:34:23 -0300

    Iniciando Link do Class Loader

---
+++ header/Link.hpp
@@ -0,0 +1,18 @@
+﻿#ifndef __Link_HPP__
+#define __Link_HPP__
+
+#include <iostream>
+#include "Dados.hpp"
+#include "ClassFile.hpp"
+
+using namespace std;
+
+class Link
+{
+public:
+    static void verify(u4 cafeBabe, u2 minorVersion, u2 majorVersion, string sourceFileFontName, string fontName);
+    static void prepare(CpInfo *cp);
+    static void resolve();
+};
+
+#endif
---
+++ src/Link.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+#include "../header/Link.hpp"
+#include "../header/Dados.hpp"
+
+using namespace std;
+
+void Link::verify(u4 cafeBabe, u2 minorVersion, u2 majorVersion, string sourceFileFontName, string fontName)
+
+{
+    if (cafeBabe != 0xCAFEBABE)
+    {
+        throw "Invalid magic number!";
+        // cout << "Invalid magic number" << endl;
+        exit(1);
+    }
+    if (minorVersion != 0)
+    {
+        throw "Invalid minor version!";
+        // cout << "Invalid minor version" << endl;
+        exit(1);
+    }
+    if (majorVersion < 45 || majorVersion > 52)
+    {
+        throw "Invalid major version!";
+        // cout << "Invalid major version" << endl;
+        exit(1);
+    }
+
+    if (sourceFileFontName != fontName)
+    {
+        throw "Invalid source file name!";
+        // cout << "Invalid source file name" << endl;
+        exit(1);
+    }
+}
