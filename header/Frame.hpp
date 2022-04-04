5a14f836b4d7a7dbd0b42b143a6014e73080917d
zFightx <alexandresc45@gmail.com>
2022-04-04 17:11:47 -0300

    Frame

---
+++ header/Frame.hpp
@@ -0,0 +1,23 @@
+#ifndef __FRAME_H__
+#define __FRAME_H__
+
+#include<iostream>
+#include<vector>
+
+#include "CpInfo.hpp"
+#include "TypeValue.hpp"
+
+class Frame
+{
+private:
+    
+public:
+    vector<Value> local_variables;
+    vector<int> operand_stack;
+    vector<CpInfo*> constant_pool;
+
+    Frame();
+    ~Frame();
+};
+
+#endif
